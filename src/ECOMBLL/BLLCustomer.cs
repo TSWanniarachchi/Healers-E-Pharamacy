52e257377f2c746a0bfbe6ac3fbee54c3ea3fab2
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-09-18 12:38:23 +0530

    Create customer business logic layer

---
+++ src/ECOMBLL/BLLCustomer.cs
@@ -0,0 +1,48 @@
+﻿using ECOMDAL;
+using System.Data;
+
+namespace ECOMBLL
+{
+    public class BLLCustomer
+    {
+        DALCustomer objDALCustomer = new DALCustomer();
+
+        public DataSet RetrieveCustomerData()
+        {
+            return objDALCustomer.RetrieveCustomerData();
+        }
+
+        public DataSet RetrieveActiveDistrict()
+        {
+            return objDALCustomer.RetrieveActiveDistrict();
+        }
+
+        public void BeginTransaction()
+        {
+            objDALCustomer.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            objDALCustomer.CommitTransaction();
+        }
+
+        public void RollbackTransaction()
+        {
+            objDALCustomer.RollbackTransaction();
+        }
+
+        public int InsertCustomerData(string name, string address, int districtId, string nicNo, bool gender, string mobileNo, string email, string userId)
+        {
+            return objDALCustomer.InsertCustomerData(name, address, districtId, nicNo, gender, mobileNo, email, userId);
+        }
+
+        public int InsertSysUserSecurityData(string userId, string password)
+        {
+            Cryptography objCryptography = new Cryptography();
+            password = objCryptography.EncryptString(password);
+
+            return objDALCustomer.InsertSysUserSecurityData(userId, password);
+        }
+    }
+}
