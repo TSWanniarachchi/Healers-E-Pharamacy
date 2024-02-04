5bc4f12e3fd8201d61b8afc439853e779735edc4
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-02-04 18:03:59 +0530

    Create stock business logic layer

---
+++ src/ECOMBLL/BLLStock.cs
@@ -0,0 +1,24 @@
+﻿using ECOMDAL;
+using System.Data;
+
+namespace ECOMBLL
+{
+    public class BLLStock
+    {
+        DALStock objDALStock = new DALStock();
+
+        public DataSet RetrieveStockDetails()
+        {
+            return objDALStock.RetrieveStockDetails();
+        }
+
+        public DataSet RetrieveStockDetailsByProduct(string proid, string itmid)
+        {
+            return objDALStock.RetrieveStockDetailsByProduct(proid,itmid);
+        }
+        public DataSet RetrieveStockDetailsByItem(string proid, string itmid)
+        {
+            return objDALStock.RetrieveStockDetailsByItem(proid, itmid);
+        }
+    }
+}
