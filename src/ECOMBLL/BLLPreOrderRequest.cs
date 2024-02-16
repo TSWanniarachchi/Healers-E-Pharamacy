035ed942873301a1b67c3c2119195d98277d8008
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-02-16 22:12:00 +0530

    Create pre order request business logic layer

---
+++ src/ECOMBLL/BLLPreOrderRequest.cs
@@ -0,0 +1,31 @@
+﻿using ECOMDAL;
+using System;
+using System.Data;
+
+namespace ECOMBLL
+{
+    public class BLLPreOrderRequest
+    {
+        DLLPreOrderRequest objDLLPreOrderRequest = new DLLPreOrderRequest();
+
+        public DataSet RetrieveOrderRequestHeader(DateTime fromDate, DateTime toDate, string orderType, int status)
+        {
+            return objDLLPreOrderRequest.RetrieveOrderRequestHeader(fromDate, toDate, orderType, status);
+        }
+
+        public DataSet RetrieveOrderRequestDetail(string orderRequestId)
+        {
+            return objDLLPreOrderRequest.RetrieveOrderRequestDetail(orderRequestId);
+        }
+
+        public int OrderConfirmation(string orderRequestId, bool confirmation, double deliveryAmount, string encryptedOTP, int rejectReasonID, string userID)
+        {
+            return objDLLPreOrderRequest.OrderConfirmation(orderRequestId, confirmation, deliveryAmount, encryptedOTP, rejectReasonID, userID);
+        }
+
+        public DataSet GetInvoiceNo(int customerId)
+        {
+            return objDLLPreOrderRequest.GetInvoiceNo(customerId);
+        }
+    }
+}
