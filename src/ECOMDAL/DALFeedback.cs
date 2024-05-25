66aab989b5ffb1dd3228a6859440a49d1e749bbe
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-05-25 21:14:20 +0530

    Create feedback data access layer

---
+++ src/ECOMDAL/DALFeedback.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ECOMDAL
+{
+    public class DALFeedback
+    {
+        DALCommon ObjDALCommon = new DALCommon();
+
+        public DataSet GetFeedback()
+        {
+            return ObjDALCommon.ExecuteProcedure_select("spSEL_Feedback");
+        }
+
+        public DataSet GetCustomer(string userId)
+        {
+            SqlParameter[] Parameters = new SqlParameter[1];
+
+            Parameters[0] = new SqlParameter("UserID", userId);
+
+            return ObjDALCommon.ExecuteProcedure_select("spSEL_CustomerByUserId", Parameters);
+        }
+
+        public int InsertFeedback(string orderRequestID, string feedback, string userId)
+        {
+            SqlParameter[] Parameters = new SqlParameter[3];
+
+            Parameters[0] = new SqlParameter("OrderRequestID", orderRequestID);
+            Parameters[1] = new SqlParameter("Feedback", feedback);
+            Parameters[2] = new SqlParameter("UserID", userId);
+
+            return ObjDALCommon.ExecuteProcedure("spINS_Feedback", CommandType.StoredProcedure, Parameters);
+        }
+    }
+}
