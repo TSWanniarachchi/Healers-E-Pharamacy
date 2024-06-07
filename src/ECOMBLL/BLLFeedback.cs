8192e8354ecdce0a2223ea859dadc879b84ba37e
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-06-07 22:41:27 +0530

    Create feedback business logic layer

---
+++ src/ECOMBLL/BLLFeedback.cs
@@ -0,0 +1,25 @@
+﻿using ECOMDAL;
+using System.Data;
+
+namespace ECOMBLL
+{
+    public class BLLFeedback
+    {
+        DALFeedback ObjDALFeedback = new DALFeedback();
+
+        public DataSet GetFeedback()
+        {
+            return ObjDALFeedback.GetFeedback();
+        }
+
+        public DataSet GetCustomer(string userId)
+        {
+            return ObjDALFeedback.GetCustomer(userId);
+        }
+
+        public int InsertFeedback(string orderRequestID, string feedback, string userId)
+        {
+            return ObjDALFeedback.InsertFeedback(orderRequestID, feedback, userId);
+        }
+    }
+}
